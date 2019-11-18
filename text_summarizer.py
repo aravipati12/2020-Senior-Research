028124a2b803bec6c7dc7f2ebb5d260557d2aa1e
Arvind Ravipati <arvindravipati1@gmail.com>
2019-11-18 09:51:53 -0500

    Add files via upload

---
+++ text_summarizer.py
@@ -0,0 +1,8 @@
+from textteaser import TextTeaser
+tt = TextTeaser()
+tt.summarize(title, text)
+
+# $ git clone https://github.com/MojoJolo/textteaser.git
+# $ sbt compile
+# $ sbt eclipse // If using Eclipse.
+# $ sbt run
