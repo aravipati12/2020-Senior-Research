4bc07c3c08dcc2fb181468c47b174fcf700e3fac
Arvind Ravipati <arvindravipati1@gmail.com>
2019-11-26 09:32:39 -0500

    Delete text_summarizer.py

--- text_summarizer.py
+++
@@ -1,9 +0,0 @@
-# from textteaser import TextTeaser
-# tt = TextTeaser()
-# tt.summarize("Summary", transcript)
-
-from gensim.summarization.summarizer import summarize
-# f = open("transcript.txt", "r")
-text = "Yesterday I went to play basketball and it was a sunny and warm day. I went with friends. Afterwards we went to get food at a restaurant. I came hom at 10 PM."
-
-print(summarize(text))
